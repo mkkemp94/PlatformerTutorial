9df5412fc20111caaecb210904c6dcf4696b9d6b
Michael Kemp <mkemp@accu-time.com>
2017-11-28 09:10:52 -0500

    Create custom game map and tile types that can be used for tiled or a custom map.

--- core/src/com/mygdx/game/world/GameMap.java
+++ core/src/com/mygdx/game/world/GameMap.java
@@ -2,6 +2,8 @@ package com.mygdx.game.world;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
+import static com.mygdx.game.world.TileType.TILE_SIZE;
+
 /**
  * Created by mkemp on 11/28/17.
  */
@@ -11,4 +13,28 @@ public abstract class GameMap {
     public abstract void render(OrthographicCamera camera);
     public abstract void update(float delta);
     public abstract void dispose();
+
+    /**
+     * Gets a time by pixel position within the game world at a specified layer.
+     * @param layer
+     * @param x
+     * @param y
+     * @return
+     */
+    public TileType getTileTypeByLocation(int layer, float x, float y) {
+        return this.getTileTypeByCoordinate(layer, (int) (x / TILE_SIZE), (int) (y / TILE_SIZE));
+    }
+
+    /**
+     * Gets a tile at its coordinate within the map at a specified layer.
+     * @param layer
+     * @param col
+     * @param row
+     * @return
+     */
+    public abstract TileType getTileTypeByCoordinate(int layer, float col, float row);
+
+    public abstract int getWidth();
+    public abstract int getHeight();
+    public abstract int getLayers();
 }
--- core/src/com/mygdx/game/world/TileType.java
+++ core/src/com/mygdx/game/world/TileType.java
@@ -1,8 +1,80 @@
 package com.mygdx.game.world;
 
+import java.util.HashMap;
+
 /**
  * Created by mkemp on 11/28/17.
  */
 
 public enum TileType {
+
+    GRASS   (1, true, "Grass"),
+    DIRT    (2, true, "Dirt"),
+    SKY     (3, false, "Sky"),
+    LAVA    (4, true, "Lava"),
+    CLOUD   (5, true, "Cloud"),
+    STONE   (6, true, "Stone");
+
+    public static final int TILE_SIZE = 16;
+
+    private int id;
+    private boolean collidable;
+    private String name;
+    private float damage;
+
+    private TileType(int id, boolean collidable, String name) {
+        this(id, collidable, name, 0);
+    }
+
+    private TileType(int id, boolean collidable, String name, float damage) {
+        this.id = id;
+        this.collidable = collidable;
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isCollidable() {
+        return collidable;
+    }
+
+    public void setCollidable(boolean collidable) {
+        this.collidable = collidable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getDamage() {
+        return damage;
+    }
+
+    public void setDamage(float damage) {
+        this.damage = damage;
+    }
+
+    // Get enum based on id
+    private static HashMap<Integer, TileType> tileMap;
+
+    static {
+        for (TileType tileType : TileType.values()) {
+            tileMap.put(tileType.getId(), tileType);
+        }
+    }
+
+    public static TileType getTileTypeById (int id) {
+        return tileMap.get(id);
+    }
 }
