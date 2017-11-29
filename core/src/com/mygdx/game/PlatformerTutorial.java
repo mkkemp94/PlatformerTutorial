dcd6746ff70aaaa4e3e82c026b0efeff0eb0ba65
Michael Kemp <mkemp@accu-time.com>
2017-11-29 09:16:16 -0500

    Render map onto screen

--- core/src/com/mygdx/game/PlatformerTutorial.java
+++ core/src/com/mygdx/game/PlatformerTutorial.java
@@ -3,17 +3,32 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.world.GameMap;
+import com.mygdx.game.world.TiledGameMap;
 
 public class PlatformerTutorial extends ApplicationAdapter {
+
+	private OrthographicCamera camera;
+
 	SpriteBatch batch;
 	Texture img;
-	
+
+	private GameMap gameMap;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		// Set up camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.update();
+
+		gameMap = new TiledGameMap();
 	}
 
 	@Override
@@ -21,6 +36,12 @@ public class PlatformerTutorial extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		if (Gdx.input.isTouched()) {
+			camera.translate(-Gdx.input.getDeltaX(), Gdx.input.getDeltaY());
+			camera.update();
+		}
+
+		gameMap.render(camera);
 	}
 	
 	@Override
---
+++ core/src/com/mygdx/game/world/TiledGameMap.java
@@ -0,0 +1,57 @@
+package com.mygdx.game.world;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+/**
+ * Created by mkemp on 11/29/17.
+ */
+
+public class TiledGameMap extends GameMap {
+
+    private TiledMap tiledMap;
+    private OrthogonalTiledMapRenderer tiledMapRenderer;
+
+    public TiledGameMap() {
+        tiledMap = new TmxMapLoader().load("map.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+    }
+
+    @Override
+    public void render(OrthographicCamera camera) {
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void dispose() {
+        tiledMap.dispose();
+    }
+
+    @Override
+    public TileType getTileTypeByCoordinate(int layer, float col, float row) {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getLayers() {
+        return 0;
+    }
+}
